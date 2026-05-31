56c82278591a8f47571306f1ea5c36342e3e5dd9
adrianvaldiosera01 <adrian.valdiosera01@gmail.com>
2026-05-31 01:03:25 -0700

    Add inventory.js to fetch and display CSV data
    Code Fetches data from Google Sheets CSV file for inventory and displays on website

---
+++ inventory.js
@@ -0,0 +1,26 @@
+const csvUrl =
+'https://docs.google.com/spreadsheets/d/e/2PACX-1vShNE87hHjBK353mmmUQQUCkoQ8CmPP56v5L-jjKrCtaBel3YtZKMVd0DU7KUsDhvXj6ckeapyKhrty/pub?output=csv';
+
+fetch(csvUrl)
+  .then(r => r.text())
+  .then(text => {
+
+    const rows = text.split('\n');
+
+    let html = '';
+
+    rows.slice(1).forEach(row => {
+
+      const cols = row.split(',');
+
+      html += `
+        <div>
+          <h3>${cols[1]} ${cols[2]} ${cols[3]}</h3>
+          <p>Price: $${cols[4]}</p>
+          <p>Miles: ${cols[5]}</p>
+        </div>
+      `;
+    });
+
+    document.getElementById('inventory').innerHTML = html;
+  });
